a2afa3362e5098c85d05b87a346375c2925ff951
Harish Mohan Raj <harish@airt.ai>
2023-04-05 21:55:01 +0530

    Fix mobile css issues

--- src/components/HomepageStayTuned/index.js
+++ src/components/HomepageStayTuned/index.js
@@ -6,31 +6,6 @@ import Form from '../Form';
 
 import styles from './styles.module.css';
 
-function Submit() {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(`Submitting value: ${inputValue}`);
-    // Perform any actions you need to with the input value
-  };
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <input className={styles.formInput} placeholder="email" type="text" value={inputValue} onChange={handleInputChange} />
-      </label>
-      <Link to="#" className="button button--secondary button--lg" onClick={handleSubmit}>
-        Dummy button
-      </Link>
-    </form>
-  );
-}
-
 export default function HomepageStayTuned() {
   return (
     <section className={clsx('hero hero--primary')}>
