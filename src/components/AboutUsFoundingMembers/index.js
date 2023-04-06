e99f7be6b83975dcf7c5ecad2838b71d19e74cbf
Harish Mohan Raj <harish@airt.ai>
2023-04-06 11:49:06 +0530

    Add social links in the contact page

--- src/components/AboutUsFoundingMembers/index.js
+++ src/components/AboutUsFoundingMembers/index.js
@@ -6,24 +6,44 @@ const FeatureList = [
   {
     title: 'Hajdi Cenan',
     imgSrc: 'img/Hajdi_profilna.png',
-    description: (
+    shortDescription: (
       <>
         Co-founder & CEO
+      </>
+    ),
+    githubProfile: "https://github.com/hajdicenan",
+    twitterProfile: "https://twitter.com/_hajdi",
+    facebookProfile: "https://www.facebook.com/hajdi.cenan",
+    linkedInProfile: "https://www.linkedin.com/in/hajdi/",
+    longDescription: (
+      <>
+        Over 20 years of experience in running operations, sales, 
+        and marketing in various industries: media, digital marketing, IT and AI.
       </>
     ),
   },
   {
     title: 'Davor Runje',
     imgSrc: 'img/Davor_profilna.png',
-    description: (
+    shortDescription: (
       <>
         Co-founder & CTO
+      </>
+    ),
+    githubProfile: "https://github.com/davorrunje",
+    twitterProfile: "https://twitter.com/davorrunje",
+    facebookProfile: "https://www.facebook.com/davor.runje",
+    linkedInProfile: "https://www.linkedin.com/in/davorrunje/",
+    longDescription: (
+      <>
+        Serial entrepreneur (2 successful exits), researcher, inventor and engineer. Over
+        25 years of experience in the IT and AI world.
       </>
     ),
   },
 ];
 
-function Feature({imgSrc, title, description}) {
+function Feature({imgSrc, title, shortDescription, longDescription, githubProfile, twitterProfile, facebookProfile, linkedInProfile}) {
   return (
     <div className={clsx('col col--6')}>
       <div className="text--center">
@@ -31,7 +51,14 @@ function Feature({imgSrc, title, description}) {
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
-        <p>{description}</p>
+        <p>{shortDescription}</p>
+        <p className={styles.linkContainer}>
+          <a className={`header-github-link ${styles.linkItems}`} href={githubProfile} target="_blank"/>
+          <a className={`header-twitter-link ${styles.linkItems}`} href={twitterProfile} target="_blank"/>
+          <a className={`header-facebook-link ${styles.linkItems}`} href={facebookProfile} target="_blank"/>
+          <a className={`header-linkedin-link ${styles.linkItems}`} href={linkedInProfile} target="_blank"/>
+        </p>
+        <p>{longDescription}</p>
       </div>
     </div>
   );
