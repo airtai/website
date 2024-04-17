fcfc264936b3a3426af3a8e8c000dc496870e699
Harish Mohan Raj <harish@airt.ai>
2024-04-17 14:17:30 +0530

    Update website copy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,56 +1,56 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'airt',
-  tagline: 'AI in motion',
+  title: "airt",
+  tagline: "AI in motion",
   customFields: {
     description:
-      'Efficient and scalable solution for building AI-powered microservices for streaming data.',
+      "Efficient and scalable solution for building AI-powered microservices for streaming data.",
   },
-  favicon: 'img/AIRT_icon_blue.svg',
+  favicon: "img/AIRT_icon_blue.svg",
 
   // Set the production url of your site here
-  url: 'http://www.airt.ai/',
+  url: "http://www.airt.ai/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'airt', // Usually your GitHub org/user name.
-  projectName: 'website', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "airt", // Usually your GitHub org/user name.
+  projectName: "website", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         gtag: {
-          trackingID: 'G-KM9N6TFVWE',
+          trackingID: "G-KM9N6TFVWE",
         },
       }),
     ],
@@ -60,80 +60,90 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/airt-social-card.png',
+      image: "img/airt-social-card.png",
       colorMode: {
-        defaultMode: 'light',
+        defaultMode: "light",
         disableSwitch: true,
       },
       navbar: {
-        title: 'airt',
+        title: "airt",
         logo: {
-          alt: 'airt logo',
-          src: 'img/AIRT_icon_blue.svg',
+          alt: "airt logo",
+          src: "img/AIRT_icon_blue.svg",
         },
         items: [
           {
-            type: 'dropdown',
-            label: 'Products',
-            position: 'right',
+            type: "dropdown",
+            label: "Products",
+            position: "right",
             items: [
               {
-                label: 'FastStream framework',
-                href: 'https://faststream.airt.ai',
-              }
-            ]
+                label: "FastAgency",
+                href: "https://fastagency.ai",
+              },
+            ],
           },
           {
-            type: 'dropdown',
-            label: 'Tools',
-            position: 'right',
+            type: "dropdown",
+            label: "Tools",
+            position: "right",
             items: [
               {
-                label: 'Monotonic Neural Networks',
-                href: 'https://monotonic.airt.ai',
+                label: "FastStream",
+                href: "https://faststream.airt.ai",
               },
               {
-                label: 'Material for nbdev',
-                href: 'https://nbdev-mkdocs.airt.ai',
-              }
-            ]
+                label: "Monotonic Neural Networks",
+                href: "https://monotonic.airt.ai",
+              },
+            ],
           },
-          {to: 'news', label: 'News', position: 'right', className: "styled-item"},
-          {to: 'about-us', label: 'About Us', position: 'right', className: "styled-item"},
           {
-            href: 'https://discord.gg/CJWmYpyFbc',
-            position: 'right',
+            to: "news",
+            label: "News",
+            position: "right",
+            className: "styled-item",
+          },
+          {
+            to: "about-us",
+            label: "About Us",
+            position: "right",
+            className: "styled-item",
+          },
+          {
+            href: "https://discord.gg/CJWmYpyFbc",
+            position: "right",
             className: "social-link extra-margin header-discord-link",
             "aria-label": "Discord Link",
           },
           {
-            href: 'https://github.com/airtai',
-            position: 'right',
+            href: "https://github.com/airtai",
+            position: "right",
             className: "social-link header-github-link",
             "aria-label": "GitHub repository",
           },
           {
-            href: 'https://twitter.com/airt_AI',
-            position: 'right',
+            href: "https://twitter.com/airt_AI",
+            position: "right",
             className: "social-link header-twitter-link",
             "aria-label": "Twitter Home Page",
           },
           {
-            href: 'https://www.facebook.com/airt.ai.api',
-            position: 'right',
+            href: "https://www.facebook.com/airt.ai.api",
+            position: "right",
             className: "social-link header-facebook-link",
             "aria-label": "Facebook Home Page",
           },
           {
-            href: 'https://www.linkedin.com/company/airt-ai',
-            position: 'right',
+            href: "https://www.linkedin.com/company/airt-ai",
+            position: "right",
             className: "social-link header-linkedin-link",
             "aria-label": "LinkedIn Home Page",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           // {
           //   title: 'Docs',
@@ -145,7 +155,7 @@ const config = {
           //   ],
           // },
           {
-            title: 'COMMUNITY',
+            title: "COMMUNITY",
             items: [
               {
                 html: `
@@ -175,45 +185,45 @@ const config = {
             ],
           },
           {
-            title: 'EXPLORE PRODUCTS',
+            title: "EXPLORE PRODUCTS",
             items: [
               {
-                label: 'FastStream framework',
-                href: 'https://faststream.airt.ai',
-              }
+                label: "FastAgency",
+                href: "https://fastagency.ai",
+              },
             ],
           },
           {
-            title: 'EXPLORE TOOLS',
+            title: "EXPLORE TOOLS",
             items: [
               {
-                label: 'Monotonic Neural Networks',
-                href: 'https://monotonic.airt.ai',
+                label: "FastStream",
+                href: "https://faststream.airt.ai",
               },
               {
-                label: 'Material for nbdev',
-                href: 'https://nbdev-mkdocs.airt.ai',
-              }
+                label: "Monotonic Neural Networks",
+                href: "https://monotonic.airt.ai",
+              },
             ],
           },
           {
-            title: 'EXPLORE MORE',
+            title: "EXPLORE MORE",
             items: [
               {
-                label: 'News',
-                to: 'news',
+                label: "News",
+                to: "news",
               },
               {
-                label: 'About Us',
-                to: 'about-us',
+                label: "About Us",
+                to: "about-us",
               },
               {
-                label: 'Company information',
-                to: 'company-information',
+                label: "Company information",
+                to: "company-information",
               },
               {
-                label: 'Contact Us',
-                to: 'contact-us',
+                label: "Contact Us",
+                to: "contact-us",
               },
             ],
           },
